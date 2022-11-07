e081e978c87d0d66711e3991e7d92d6d9ccee4b9
Maciek <msitkowski94@gmail.com>
2022-11-07 20:29:29 +0100

    implement pig latin function

--- lectures/pig-latin.scm
+++ lectures/pig-latin.scm
@@ -1,7 +1,16 @@
 (load "_simply.scm")
 
-; TODO: Moves part of the word up to first vowel to the and of the word and appends "ay"
-(define (pigl wd)
-     (if (equal? wd 'password)
-        'true
-        'false))
+; Moves part of the word up to first vowel to the and of the word and appends "ay"
+(define (pigl input)
+      ; first letter is vowel
+      (if (vowel? (first input))
+
+         ; return word with appended "ay" at the end 
+         (word input 'ay)
+
+         ; else, move first letter to the end, and call pigl again
+         (pigl (word (bf input) (first input)))))
+
+; Check if given letter is a vowel. It does NOT handle "Y" for now.
+(define (vowel? letter)
+   (member? letter '(a e i o u)))
