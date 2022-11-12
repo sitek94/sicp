0740c38bfcf3df3f1e8e1ae32ccc07489c069090
Maciek <msitkowski94@gmail.com>
2022-11-12 23:10:21 +0100

    add checkmark button and integrate Mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import {defineConfig} from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-})
+  integrations: [tailwind(), mdx()]
+});
