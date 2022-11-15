19f9cbad940b408274d97a0b161e1233c2fa6047
Maciek <msitkowski94@gmail.com>
2022-11-15 20:56:59 +0100

    solve 2nd ex

---
+++ lectures/squares.scm
@@ -0,0 +1,9 @@
+(load "_simply.scm")
+(load "square.scm")
+
+(define (squares list)
+    (if (is-empty? list) list
+        (se (square (first list)) (squares (bf list)))))
+
+(define (is-empty? list)
+    (= (count list) 0))
